c11c6080c5e798629d0d4f9ef1e419dbaec4d7ea
lusine5555 <lusine@ultralabs.io>
2025-06-27 21:41:32 +0400

    improvement

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const cors = require('cors');
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://photo-editor-git-master-lus-projects-b20ea231.vercel.app', 
+    credentials: true 
+  }));
 app.use(express.json());
 
 app.use('/api', authRoutes);
